9fc009be8b312b82aebc1a8c83b902cd9e9809c8
oumaimaezarouali <oumaima.2001ezarouali@gmail.com>
2024-05-07 20:08:11 +0000

    first Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+import Todolist from "./components/Todolist";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Todolist/>
+    </>
+  )
+}
